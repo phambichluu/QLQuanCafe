e17a97cdcfe0b3176b6d4df7945033f7ed41a7c3
THUY QUYNH DANG THI <1821050208@student.humg.edu.vn>
2021-04-10 17:34:36 +0700

    LTQL

---
+++ QLQuanCafe/Models/Ngay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace QLQuanCafe.Models
+{
+    [Table("Ngays")]
+    public class Ngay
+    {
+        [Key]
+        public string Ngay { get; set; }
+    }
+}
